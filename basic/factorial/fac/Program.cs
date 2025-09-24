a0d15a42ac7ab7eb20d79af47cffb8b7901a8372
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-24 13:39:35 +0300

    nw

---
+++ basic/factorial/fac/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace fac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to factorial app");
+            int fact=1;
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(n == 0)
+            {
+                Console.WriteLine(1);
+            }else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    fact *= i;
+                }
+                Console.WriteLine($"{n}'s factorial is {fact}");
+            }
+                
+        }
+    }
+}
---
+++ basic/fizz_buzz/fizz_buzz/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace fizz_buzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to FizzBuzz app");
+            for(int i = 0; i< 101;i++)
+            {
+                if( i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
