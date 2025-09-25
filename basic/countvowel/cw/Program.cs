ee89488dd53e0bf227c58dc5ce5e32ce1a8ecdd0
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-25 10:57:04 +0300

    countwovel

---
+++ basic/countvowel/cw/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace cw
+{
+    internal class Program
+    {
+        public  static int CountVowels(string text)
+        {
+            int count = 0;
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (text[i].Equals('a') || text[i].Equals('e')|| text[i].Equals('i') || text[i].Equals('o') || text[i].Equals('u'))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountVowels("Hello World")); //3
+        }
+    }
+}
