ef950d2c5a62f58a9a5d97e8032909dbbd85a3de
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-25 11:13:34 +0300

    strevers

---
+++ basic/string-reverse/sr/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace sr
+{
+    internal class Program
+    {
+        public static string ReverseString(string Text)
+        {
+            string reversed = "";
+            for(int i = Text.Length -1; i>= 0;i--)
+            {
+                reversed += Text[i];
+            }
+            return reversed;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseString("Burak"));
+        }
+    }
+}
