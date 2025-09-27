f46bb36a8b48b09107feabc3b53c535628b47d9c
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-09-27 12:49:17 +0300

    oop exercise

---
+++ oop/oopexercise/Shape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopexercise
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();   
+    }
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public override double CalculateArea()
+        {
+            double Area = Width * Height;
+            return Area;
+        }
+    }
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public override double CalculateArea()
+        {
+            double Area = Math.PI * Radius * Radius;
+            return Area;
+        }
+    }
+}
---
+++ oop/shape2/Shape.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape2
+{
+    public abstract class Shape
+    {
+        public abstract double CalculateArea();
+
+    }
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public override double CalculateArea()
+        {
+            return Width * Height;
+        }
+        public class Circle : Shape
+        {
+            public double Radius { get; set; }
+            public override double CalculateArea()
+            {
+                return Math.PI * Radius * Radius;
+            }
+        }
+        public class Triangle : Shape
+        {
+            public double Base { get; set; }
+            public double Height { get; set; }
+            public override double CalculateArea()
+            {
+                double Area = 0.5 * Base * Height;
+                return Area;
+            }
+        }
+        public class ShapeDrawer 
+        {
+            public static void DrawShapeArea(Shape shape)
+            {
+                double Area = shape.CalculateArea();
+                Console.WriteLine($"The area is {Area}");
+            }
+        }
+    }
+}
