0cf638064c20a2cd0f34d0c83d1c03a4a7a65668
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-10-09 14:23:34 +0300

    araeanagram

---
+++ basic/areAnagram/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace areAnagram
+{
+    internal class Program
+    {
+        public bool AreAnagrams(string firstWord, string secondWord)
+        {
+            if (firstWord.Length != secondWord.Length)
+                return false;
+
+            char[] a = firstWord.ToLower().ToCharArray();
+            char[] b = secondWord.ToLower().ToCharArray();
+            Array.Sort(a);
+            Array.Sort(b);
+            return new string(a) == new string(b);
+           
+        }
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
