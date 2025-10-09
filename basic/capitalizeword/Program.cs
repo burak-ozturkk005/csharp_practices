3cbcf4d53adabf11ddf3a232decc0718778687ed
Burak Ozturk <burak.ozturkk05@gmail.com>
2025-10-09 14:24:30 +0300

    capword

---
+++ basic/capitalizeword/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace capitalizeword
+{
+    internal class Program
+    {
+        public static string Capitalize(string sentence)
+        {
+            char[] chars = sentence.ToCharArray();
+                for(int i = 0; i< chars.Length;i++)
+            {
+                if(i == 0 || chars[i - 1] == ' ')
+                {
+                    chars[i] = char.ToUpper(chars[i]);
+                }
+            }
+            string result = new string(chars);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Capitalize("burak ozturk"));
+        }
+    }
+}
